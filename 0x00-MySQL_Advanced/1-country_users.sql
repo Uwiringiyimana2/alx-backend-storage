61643535173d5e5ab0205dfc6bdd8209d2f53e9e
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-10 10:19:07 -0700

    1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a table users With these attributes:
+-- id, email, name, country
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+  PRIMARY KEY (id)
+);
