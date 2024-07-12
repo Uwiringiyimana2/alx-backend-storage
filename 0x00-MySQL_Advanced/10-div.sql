562a580961ecf06fe8b291edd09e449cdcb3f566
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-12 03:03:11 -0700

    10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- creates a function SafeDiv
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+DETERMINISTIC
+BEGIN
+  IF b = 0 THEN
+    RETURN 0;
+  ELSE
+    RETURN a / b;
+  END IF;
+END$$
