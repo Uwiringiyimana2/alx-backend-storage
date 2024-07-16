e0c4f0fa809881f486fe768044b66c67d2375a67
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-16 13:38:46 -0700

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""provides some stats about Nginx logs stored in MongoDB"""
+import pymongo
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collections = client.logs.nginx
+    x = nginx_collections.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{x} logs")
+    print("Methods:")
+    for method in methods:
+        res = nginx_collections.count_documents({"method": method})
+        print(f"\tmethod {method}: {res}")
+
+    status_count = nginx_collections.count_documents(
+            {"method": "GET", "path": "/status"})
+    print("{} status check".format(status_count))
+
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_ips = nginx_collections.aggregate(pipeline)
+    
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
