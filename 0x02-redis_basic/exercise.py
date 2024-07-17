d7fbc375ce43ce9cc8b6be4e7d028da131ee03f4
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-17 08:41:27 -0700

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -20,3 +20,21 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+    
+    def get(self, key: str, fn: Optional[Collable] = None) -> Union[str, bytes, int, float, None]:
+        """Retrieve data from Redis, optionally converting it
+        using a provided function."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve a string from Redis"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve an integer from Redis"""
+        return self.get(key, fn=int)
