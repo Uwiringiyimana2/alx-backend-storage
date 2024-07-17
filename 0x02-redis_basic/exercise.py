4ad9862e82da4636537431c7d664b4ff33b3f216
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-17 11:25:08 -0700

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,20 @@
 """Writing strings to Redis"""
 import redis
 import uuid
+import functools
 from typing import Union, Optional, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator that counts the number of calls to the method"""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function to increment the call count
+        and call the original method"""
+        key = f"{method.__qualname__}:calls"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -14,14 +27,17 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store the input data in Redis using the
         random key and return the key."""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
-    def get(self, key: str, fn: Optional[Collable] = None) -> Union[str, bytes, int, float, None]:
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
         """Retrieve data from Redis, optionally converting it
         using a provided function."""
         data = self._redis.get(key)
@@ -30,7 +46,7 @@ class Cache:
         if fn is not None:
             return fn(data)
         return data
-    
+
     def get_str(self, key: str) -> Optional[str]:
         """Retrieve a string from Redis"""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
